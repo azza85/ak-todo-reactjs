98570b35ac8e1552fa0f5b3005cf522b634361bb
aaron <aaronkenez@gmail.com>
2020-02-07 22:20:45 +1100

    add in Readme

---
+++ src/__tests__/mockTodos.js
@@ -0,0 +1,17 @@
+export const mockTodos = {
+  1: {
+    name: "Richard",
+    isComplete: false,
+    priority: 3
+  },
+  2: {
+    name: "Kate",
+    isComplete: true,
+    priority: 1
+  },
+  3: {
+    name: "Sam",
+    isComplete: false,
+    priority: 2
+  }
+};
---
+++ src/__tests__/sortAtoZ.js
@@ -0,0 +1,13 @@
+import { sortAtoZ } from "./../helpers/sortAtoZ";
+import { mockTodos } from "./mockTodos";
+
+const toDoList = Object.keys(mockTodos);
+describe("sortAtoZ", () => {
+  const expected = ["Kate", "Richard", "Sam"];
+  it("sort array alphabetically from a to z", () => {
+    const outputAsArray = toDoList
+      .sort(sortAtoZ("name", mockTodos))
+      .map(item => mockTodos[item].name);
+    expect(outputAsArray).toEqual(expect.arrayContaining(expected));
+  });
+});
